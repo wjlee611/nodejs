aa4063eef9015afc6728942250495cc4cd7b4be2
wjlee611 <wjlee611@gmail.com>
2022-07-13 23:58:35 +0900

    update post

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
-      <a href="/create">Create</a>
+      ${control}
       ${body}
     </body>
   </html>
@@ -49,7 +49,8 @@ var app = http.createServer(function (request, response) {
           title,
           list,
           `<h2>${title}</h2>
-          <p>${description}</p>`
+          <p>${description}</p>`,
+          `<a href="/create">Create</a>`
         );
         response.writeHead(200);
         response.end(template);
@@ -65,7 +66,9 @@ var app = http.createServer(function (request, response) {
               title,
               list,
               `<h2>${title}</h2>
-              <p>${description}</p>`
+              <p>${description}</p>`,
+              `<a href="/create">Create</a>
+              <a href="/update?id=${title}">Update</a>`
             );
             response.writeHead(200);
             response.end(template);
@@ -81,12 +84,13 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-        <form action="http://localhost:3000/create_process" method="post">
+        <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title" /></p>
         <p><textarea name="description" placeholder="description"></textarea></p>
         <p><input type="submit" /></p>
         </form>
-        `
+        `,
+        ""
       );
       response.writeHead(200);
       response.end(template);
@@ -103,6 +107,45 @@ var app = http.createServer(function (request, response) {
       fs.writeFile(`data/${title}`, description, "utf-8", function (err) {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end("success");
+      });
+    });
+  } else if (pathName === "/update") {
+    fs.readdir("./data", function (error, filelist) {
+      var list = templateList(filelist);
+      fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+        var template = templateHTML(
+          title,
+          list,
+          `
+          <form action="/update_process" method="post">
+          <input type="hidden" name="id" value=${title} />
+          <p><input type="text" name="title" placeholder="title" value=${title}/></p>
+          <p><textarea name="description" placeholder="description">${description}</textarea></p>
+          <p><input type="submit" /></p>
+          </form>
+          `,
+          `<a href="/create">Create</a>
+            <a href="/update?id=${title}">Update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if (pathName === "/update_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description = post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function (err) {
+        fs.writeFile(`data/${title}`, description, "utf-8", function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end("success");
+        });
       });
     });
   } else {
