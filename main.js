737a673baafdedcb0bff9ee45c2641d1a888e9cc
wjlee611 <wjlee611@gmail.com>
2022-07-13 23:41:12 +0900

    writefile and redirect

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var qs = require("querystring");
 
 function templateHTML(title, list, body) {
   return `
   <!doctype html>
   <html>
     <head>
-      <title>WEB1 - ${title}</title>
+      <title>${title}</title>
       <meta charset="utf-8">
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/create">Create</a>
       ${body}
     </body>
   </html>
@@ -71,6 +73,38 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (pathName === "/create") {
+    fs.readdir("./data", function (error, filelist) {
+      title = "create";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title" /></p>
+        <p><textarea name="description" placeholder="description"></textarea></p>
+        <p><input type="submit" /></p>
+        </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathName === "/create_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, "utf-8", function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end("success");
+      });
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
